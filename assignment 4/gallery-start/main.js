1b58e67290b8d22f52e9e0c399ddfec0565e9e63
zaryabkhan47 <zaryab.khan@dcmail.ca>
2023-11-22 13:40:41 -0500

    Declared alternative text for every image

--- assignment 4/gallery-start/main.js
+++ assignment 4/gallery-start/main.js
@@ -1,1 +1,10 @@
  const imageFilenames = ['pic1.jpg' , 'pic2.jpg' , 'pic3.jpg' , 'pic4.jpg' , 'pic5.jpg'];
+/* Declaring the alternative text for each image file */
+
+const altTexts = [
+'Description for pic1',
+'Description for pic2',
+'Description for pic3',
+'Description for pic4',
+'Description for pic5'
+];
