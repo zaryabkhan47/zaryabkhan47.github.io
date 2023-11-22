692f09ff5e10d20a539ed338a5b4dbce276aa010
zaryabkhan47 <zaryab.khan@dcmail.ca>
2023-11-22 18:25:31 -0500

    main-finished raw file

---
+++ assignment 4/assignment 4-p4/main-finished.js
@@ -0,0 +1,59 @@
+// setup canvas
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// function to generate random number
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random RGB color value
+function randomRGB() {
+  return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+}
+
+// Shape class
+class Shape {
+  constructor(x, y, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+  }
+}
+
+
+
+
+// Balls array
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  let ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+  balls.push(ball);
+}
+
+// Initialize the EvilCircle instance
+const evilCircle = new EvilCircle(random(0, width), random(0, height));
+
+// Animation loop
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+
+
+  requestAnimationFrame(loop);
+}
+
+loop();
