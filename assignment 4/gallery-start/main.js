e1919be09c0a910a09c2161aa2674d0c8a7890a5
zaryabkhan47 <zaryab.khan@dcmail.ca>
2023-11-22 14:04:49 -0500

    Add Darken/Lighten toggle to button with overlay effect

--- assignment 4/gallery-start/main.js
+++ assignment 4/gallery-start/main.js
@@ -29,4 +29,9 @@ if (currentClass === 'dark') {
     btn.setAttribute('class' , 'light');
     btn.textContent = 'Lighten';
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)'
+} else {
+btn.setAttribute('class', 'dark');
+btn.textContent = 'Darken';
+overlay.style.backgroundColor = 'rgba(0,0,0,0)'
+
 }
