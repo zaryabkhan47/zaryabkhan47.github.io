3502d55428c12c00c7f1881d98dec73f3aa52325
zaryabkhan47 <zaryab.khan@dcmail.ca>
2023-11-22 18:33:18 -0500

    Implement EvilCircle class with movement, boundary checks, and collision logic

--- assignment 4/assignment 4-p4/main-finished.js
+++ assignment 4/assignment 4-p4/main-finished.js
@@ -68,6 +68,66 @@ class Ball extends Shape {
   }
 }
 
+// EvilCircle class that extends Shape
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = 'red';  // Changed color for visibility
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+  draw() {
+   ctx.beginPath();
+   ctx.lineWidth = 3;
+   ctx.strokeStyle = this.color;
+   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+   ctx.stroke();
+ }
+
+ checkBounds() {
+   if ((this.x + this.size) >= width) {
+     this.x -= this.size;
+   } else if ((this.x - this.size) <= 0) {
+     this.x += this.size;
+   }
+
+   if ((this.y + this.size) >= height) {
+     this.y -= this.size;
+   } else if ((this.y - this.size) <= 0) {
+     this.y += this.size;
+   }
+ }
+
+ collisionDetect() {
+   for (const ball of balls) {
+     if (ball.exists) {
+       const dx = this.x - ball.x;
+       const dy = this.y - ball.y;
+       const distance = Math.sqrt(dx * dx + dy * dy);
+
+       if (distance < this.size + ball.size) {
+         ball.exists = false;
+       }
+     }
+   }
+ }
+}
 
 
 // Balls array
