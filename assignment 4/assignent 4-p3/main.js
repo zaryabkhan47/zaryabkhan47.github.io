f9473b457f989e55b9e96db6fef88ae7479005bc
zaryabkhan47 <zaryab.khan@dcmail.ca>
2023-11-22 15:08:16 -0500

    Implement ball movement and boundary collision logic in Ball class

--- assignment 4/assignent 4-p3/main.js
+++ assignment 4/assignent 4-p3/main.js
@@ -17,3 +17,12 @@ ctx.fillStyle = this.color;
 ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
 ctx.fill();
 
+ update() {
+if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+  this.velX = -(this.velX);
+}
+if ((this.y + this.size) >= height || (this.y -this.size) <= 0) {
+  this.velY = -(this.velY);
+}
+this.x += this.velX;
+this.y += this.velY;
