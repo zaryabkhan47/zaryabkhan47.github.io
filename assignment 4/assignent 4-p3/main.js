316055b097cc8fa764228fd51319feef05c0ab92
zaryabkhan47 <zaryab.khan@dcmail.ca>
2023-11-22 15:19:50 -0500

    Add collision detection and color change in Ball class

--- assignment 4/assignent 4-p3/main.js
+++ assignment 4/assignent 4-p3/main.js
@@ -26,3 +26,16 @@ if ((this.y + this.size) >= height || (this.y -this.size) <= 0) {
 }
 this.x += this.velX;
 this.y += this.velY;
+collisionDetect () {
+  for (const ball of balls) {
+    if (!(this === ball)) {
+      const dx = this.x - ball.x;
+      const dy = this.y - ball.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + ball.size) {
+        ball.color = this.color = randomRGB ();
+      }
+    }
+  }
+}
