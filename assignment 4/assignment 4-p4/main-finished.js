7ee3a701aab66ccbae903169acfa90730efcb894
zaryabkhan47 <zaryab.khan@dcmail.ca>
2023-11-22 18:31:10 -0500

    Enhance collisionDetect in Ball class to check existence and change color

--- assignment 4/assignment 4-p4/main-finished.js
+++ assignment 4/assignment 4-p4/main-finished.js
@@ -54,6 +54,19 @@ class Ball extends Shape {
     }
   }
 
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
 
 
 
