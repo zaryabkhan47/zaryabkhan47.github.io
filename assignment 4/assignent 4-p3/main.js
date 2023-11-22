7bd2cfea1ca247e6eff76625cb4ec95c07c679c9
zaryabkhan47 <zaryab.khan@dcmail.ca>
2023-11-22 14:56:09 -0500

    Create Ball class for modeling ball attributes and behavior

--- assignment 4/assignent 4-p3/main.js
+++ assignment 4/assignent 4-p3/main.js
@@ -1,2 +1,13 @@
 function random(min, max) {
 return Math.floor(Math.random() * (max - min + 1)) + min;
+// Modeling a Ball
+
+class Ball {
+  constructor (x, y, velX, velY, color, size) {
+this.x =x;
+this.y =y;
+this.velX = velX;
+this. velY = velY;
+this.color=color;
+this.size = size ;
+
