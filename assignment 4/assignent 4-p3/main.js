2f49ea713059c46ac4ea799b924f858e4e6f9f6b
zaryabkhan47 <zaryab.khan@dcmail.ca>
2023-11-22 15:30:42 -0500

    Initialize and animate balls with draw, update, and collision logic

--- assignment 4/assignent 4-p3/main.js
+++ assignment 4/assignent 4-p3/main.js
@@ -39,3 +39,26 @@ collisionDetect () {
     }
   }
 }
+// Animation of balls
+const balls = [];
+while (balls.length <25) {
+  const size = random(10,20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+  balls.push(ball);
+}
+function loop(){
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width,height);
+
+  for(const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
